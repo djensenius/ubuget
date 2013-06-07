96fd0a2ec19e97afb0f7c22c4bbc608358d92c90
David Jensenius <djensenius@gmail.com>
2013-06-07 10:46:56 -0400

    A little bit of cleanup

--- ubuget.sh
+++ ubuget.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-#################################################
-#	Simple script to download files from		#
-#	UBU.com.									#
-#												#
-#	./ubuget.sh UBUURL							#
-#												#
-#												#
-#	2012 David Jensenius						#
-#################################################
+#	Simple script to download files from UBU.com to an appropriately named directory.
+#		
+#	Example	
+#	./ubuget.sh http://www.ubu.com/sound/kagel_match.html	
+#	
+#	2013 David Jensenius
 
 
 if [ $# != 1 ]; then
